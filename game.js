71c4b0cfa3b5a9fbcdd9d696bcc1012934ec7595
Martin McElrath <mmcelrath@proton.me>
2023-12-03 07:16:56 -0500

    initial commit with basic logic spec

---
+++ game.js
@@ -0,0 +1,14 @@
+// Generate computer choice from Rock, Paper or Scissors
+function getComputerChoice() {
+
+}
+
+// Take player input and evaluate against computer choice following RPS rules
+function playRound() {
+
+}
+
+// Play 5 rounds of RPS, declare winner, and display results
+function game() {
+
+}
