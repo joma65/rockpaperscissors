9f737e230f6214ebe566dc41355905af4b1d2207
Martin McElrath <mmcelrath@proton.me>
2023-12-03 08:33:49 -0500

    Logic complete, initial webpage. todo - images

--- game.js
+++ game.js
@@ -1,14 +1,63 @@
+const choices = ["rock", "paper", "scissors"];
+let playerInput = "";
+let computerChoice = "";
+let playerScore = 0;
+let computerScore = 0;
+let started = false;
+
+$("img").on("click",function () {
+    playerInput = $(this).attr("id");
+    $(playerInput).fadeOut().fadeIn();
+    console.log(playerInput);
+    game(playerInput);
+});
+
+$("#reset").on("click",reset);
+
 // Generate computer choice from Rock, Paper or Scissors
 function getComputerChoice() {
-
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)]
+    $("#computerChoice").attr("src", `images/${computerChoice}.png`);
+    return computerChoice;
 }
 
 // Take player input and evaluate against computer choice following RPS rules
-function playRound() {
+function playRound(playerInput) {
+    computerChoice = getComputerChoice();
+    if (playerInput === computerChoice) {
+        $(".score").text("Tie round, choose again!");
+    } else if (
+        (playerInput === "rock" && computerChoice === "scissors") ||
+        (playerInput === "paper" && computerChoice === "rock") ||
+        (playerInput === "scissors" && computerChoice === "paper"))  {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
+// Play 5 rounds of RPS, declare winner
+function game(playerInput) {
+    let round = playRound(playerInput);
+    if (round === true) {
+        playerScore++;
+        $(".score").text(`${playerScore} - ${computerScore}`);
+    } else if (round === false) {
+        computerScore++;
+        $(".score").text(`${playerScore} - ${computerScore}`);
+    }
+    if (playerScore + computerScore === 5) {
+        if (playerScore > computerScore) {
+            console.log(`You win! Final score: ${playerScore} - ${computerScore}`);
+        } else if (playerScore < computerScore) {
+            console.log(`You lose! Final score: ${playerScore} - ${computerScore}`);
+        }
+    }
 }
 
-// Play 5 rounds of RPS, declare winner, and display results
-function game() {
-
+function reset() {
+    console.log("reset");
+    let playerScore = 0;
+    let computerScore = 0;
+    $(".score").text(`Score reset`);
 }
