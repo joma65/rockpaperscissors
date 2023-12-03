2d290a2c08b5afa1b21840fb0179e39923d3fe91
joma65 <mmcelrath@proton.me>
2023-12-03 14:32:23 -0500

    add images, finish logic

--- game.js
+++ game.js
@@ -1,63 +1,68 @@
 const choices = ["rock", "paper", "scissors"];
+$(".bottomContainer").hide();
 let playerInput = "";
 let computerChoice = "";
 let playerScore = 0;
 let computerScore = 0;
 let started = false;
 
-$("img").on("click",function () {
-    playerInput = $(this).attr("id");
-    $(playerInput).fadeOut().fadeIn();
-    console.log(playerInput);
-    game(playerInput);
+$("img").on("click", function () {
+	playerInput = $(this).attr("id");
+	$(this).fadeOut(100).fadeIn(100);
+	game(playerInput);
 });
 
-$("#reset").on("click",reset);
+$("#reset").on("click", reset);
 
 // Generate computer choice from Rock, Paper or Scissors
 function getComputerChoice() {
-    let computerChoice = choices[Math.floor(Math.random() * choices.length)]
-    $("#computerChoice").attr("src", `images/${computerChoice}.png`);
-    return computerChoice;
+	computerChoice = choices[Math.floor(Math.random() * choices.length)];
+	$("#computerChoice").attr("src", `images/${computerChoice}.png`);
+    if (started === false) {
+        started = true;
+        $("#start").hide();
+        $(".bottomContainer").show();
+    }
+	return computerChoice;
 }
 
 // Take player input and evaluate against computer choice following RPS rules
 function playRound(playerInput) {
-    computerChoice = getComputerChoice();
-    if (playerInput === computerChoice) {
-        $(".score").text("Tie round, choose again!");
-    } else if (
-        (playerInput === "rock" && computerChoice === "scissors") ||
-        (playerInput === "paper" && computerChoice === "rock") ||
-        (playerInput === "scissors" && computerChoice === "paper"))  {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	computerChoice = getComputerChoice();
+	if (playerInput === computerChoice) {
+        return;
+	} else if (
+		(playerInput === "rock" && computerChoice === "scissors") ||
+		(playerInput === "paper" && computerChoice === "rock") ||
+		(playerInput === "scissors" && computerChoice === "paper")
+	) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
 // Play 5 rounds of RPS, declare winner
 function game(playerInput) {
-    let round = playRound(playerInput);
-    if (round === true) {
-        playerScore++;
-        $(".score").text(`${playerScore} - ${computerScore}`);
-    } else if (round === false) {
-        computerScore++;
-        $(".score").text(`${playerScore} - ${computerScore}`);
-    }
-    if (playerScore + computerScore === 5) {
-        if (playerScore > computerScore) {
-            console.log(`You win! Final score: ${playerScore} - ${computerScore}`);
-        } else if (playerScore < computerScore) {
-            console.log(`You lose! Final score: ${playerScore} - ${computerScore}`);
-        }
+	let round = playRound(playerInput);
+
+	if (round === true) {
+		playerScore++;
+        $(".score").text(`YOU: ${playerScore} - CPU: ${computerScore}`);
+	} else if (round === false) {
+		computerScore++;
+        $(".score").text(`YOU: ${playerScore} - CPU: ${computerScore}`);
+	} else {
+        $(".score").text("Tie round, choose again!");
     }
 }
 
 function reset() {
-    console.log("reset");
-    let playerScore = 0;
-    let computerScore = 0;
-    $(".score").text(`Score reset`);
+	playerInput = "";
+	computerChoice = "";
+	playerScore = 0;
+	computerScore = 0;
+    started = false;
+    $("#start").show();
+	$(".bottomContainer").hide();
 }
